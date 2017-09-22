7bec42bca29e8c2bcfe7343d4b0e6740c0c12624
ParagChandakkar <pchandak@asu.edu>
2017-09-22 16:38:06 -0700

    Bug fixes, working copy

--- pytorch-pruning-master/move_data.py
+++ pytorch-pruning-master/move_data.py
@@ -1,14 +1,19 @@
 import glob
 import os, sys, shutil
 import random
+import numpy as np
 
-def moveFiles(dir_from, dir_to, numFiles=-1):
-    # if numFiles=-1, all files will be moved
-    # if numFiles>0, random numFiles files will be moved
+np.random.seed(10000)
 
-    fullPaths_from = glob.glob(dir_from)
+def moveFiles(dir_from, dir_to, fileType=None, numFilesToKeepInSrc=-1):
+    # if numFiles=-1, no files will be kept in dir_from
+    # if numFiles>0, random numFilesToKeepInSrc files will be kept in dir_from
+
+    fullPaths_from = glob.glob(dir_from + '*.' + fileType)
     fullPaths_to = [dir_to + p.split('/')[-1] for p in fullPaths_from]
-    l = random.shuffle(range(len(fullPaths_from)))
+    l = range(len(fullPaths_from))
+    random.shuffle(l)
+    l = l[:len(fullPaths_from) - numFilesToKeepInSrc]
     for i in l:
         shutil.move(fullPaths_from[i], fullPaths_to[i])
 
@@ -18,5 +23,5 @@ cats_dir_to = '/home/vijetha/Dropbox (ASU)/Pruning_Retraining/pytorch-pruning-ma
 dogs_dir_from = '/home/vijetha/Dropbox (ASU)/Pruning_Retraining/pytorch-pruning-master/train/dogs/'
 dogs_dir_to = '/home/vijetha/Dropbox (ASU)/Pruning_Retraining/pytorch-pruning-master/train/dogs_extra/'
 
-moveFiles(cats_dir_from, cats_dir_to)
-moveFiles(dogs_dir_from, dogs_dir_to)
+moveFiles(cats_dir_from, cats_dir_to, fileType='jpg', numFilesToKeepInSrc=2000)
+moveFiles(dogs_dir_from, dogs_dir_to, fileType='jpg', numFilesToKeepInSrc=2000)
